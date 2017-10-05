358024dde34aceb0cdfee0b3af176db733d7805e
CarminaP <carminita_perez@hotmail.com>
2017-10-04 19:18:54 -0500

    hilo-con-semaforo

--- threads/hello_world.c
+++ threads/hello_world.c
@@ -1,12 +1,25 @@
 #include <pthread.h>
  #include <stdio.h>
- #define NUM_THREADS     5
+ #include <unistd.h>
+ #include <semaphore.h>
+ #define NUM_THREADS     10
+
+ long saldo;
+ sem_t mutex;
 
  void *PrintHello(void *threadid)
  {
     long tid;
+    long lsaldo;
     tid = (long)threadid;
     printf("Hello World! It's me, thread #%ld!\n", tid);
+    sem_wait(&mutex);
+    lsaldo = saldo;
+    sleep(1); //Forzando a perder el CPU
+    lsaldo += 1;
+    saldo = lsaldo;
+    sem_post(&mutex);
+
     pthread_exit(NULL); //valor de retorno que atraparia el main
  }
 
@@ -15,6 +28,10 @@
     pthread_t threads[NUM_THREADS]; //para identificar al hilo
     int rc;
     long t;
+
+    saldo = 0;
+    sem_init(&mutex,0,1);
+
     for(t=0; t<NUM_THREADS; t++){
        printf("In main: creating thread %ld\n", t);
        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
@@ -23,7 +40,10 @@
           exit(-1);
        }
     }
-
+    for(t=0; t<NUM_THREADS; t++){
+      pthread_join(threads[t],NULL);
+    }
+    printf("El valor del saldo es %ld\n", saldo);
     /* Last thing that main() should do */
     pthread_exit(NULL);
  }
